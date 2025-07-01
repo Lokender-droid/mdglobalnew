86cdedf9162f60f10e7d593fca1156e689ce6281
Lokender-droid <slokender05@gmail.com>
2025-07-01 15:16:16 +0530

    Add ScrollToTop component and integrate into AppLayout

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from "./../HomePage/NavBar";
 import { Outlet } from 'react-router-dom'
 import FooterPage from "./../PageFooter/FooterPage";
+import ScrollToTop from "../ScrollToTop";
 
 
 const AppLayout = () => {
   return (
       <div className=''>
        <NavBar></NavBar>   
+       <ScrollToTop /> {/* ✅ This scrolls to top on every route change */}
        <Outlet/>
       <FooterPage></FooterPage>
     </div>
